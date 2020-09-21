8c764be27175c30f8adf002da278fc695164cedc
Fatih Can <ybsfatihcan@gmail.com>
2020-09-21 10:18:52 +0300

    upload data

---
+++ app/src/main/java/com/example/noteapp/db/Note.kt
@@ -0,0 +1,14 @@
+package com.example.noteapp.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note(
+    val title: String,
+    val note: String
+){
+    @PrimaryKey(autoGenerate = true)
+    var id : Int = 0
+}
--- app/src/main/java/com/example/noteapp/ui/AddNoteFragment.kt
+++ app/src/main/java/com/example/noteapp/ui/AddNoteFragment.kt
@@ -1,11 +1,16 @@
 package com.example.noteapp.ui
 
+import android.os.AsyncTask
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.noteapp.R
+import com.example.noteapp.db.Note
+import com.example.noteapp.db.NoteDatabase
+import kotlinx.android.synthetic.main.fragment_add_note.*
 
 
 class AddNoteFragment : Fragment() {
@@ -19,5 +24,47 @@ class AddNoteFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_add_note, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
+        button_save.setOnClickListener {
+
+            val noteTitle = edit_text_title.text.toString().trim()
+            val noteBody = edit_text_note.text.toString().trim()
+
+            if (noteTitle.isEmpty()){
+                edit_text_title.error = "title required"
+                edit_text_title.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (noteBody.isEmpty()){
+                edit_text_note.error = "note required"
+                edit_text_note.requestFocus()
+                return@setOnClickListener
+            }
+
+            val note = Note(noteTitle, noteBody)
+            saveNote(note)
+
+        }
+    }
+
+    private fun saveNote(note:Note){
+        class SaveNote : AsyncTask<Void, Void, Void>(){
+
+            override fun doInBackground(vararg p0: Void?): Void? {
+                NoteDatabase(requireActivity()).getNoteDao().addNote(note)
+                return null
+            }
+
+            override fun onPostExecute(result: Void?) {
+                super.onPostExecute(result)
+
+                Toast.makeText(activity, "Note Saved", Toast.LENGTH_LONG).show()
+            }
+
+        }
+        SaveNote().execute()
+    }
 }
